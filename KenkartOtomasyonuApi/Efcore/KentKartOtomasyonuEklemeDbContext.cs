209962bd1eeeb01f0d9829fafa48ba3a7ee77dbb
Umutcan Kemahlı <130008707+RagnarLorthbro@users.noreply.github.com>
2024-09-09 19:42:29 +0300

    Proje dosyası ekle.

---
+++ KenkartOtomasyonuApi/Efcore/KentKartOtomasyonuEklemeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using KenkartOtomasyonuApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KenkartOtomasyonuApi.Efcore
+{
+    public class KentKartOtomasyonuEklemeDbContext :DbContext
+    {
+        public DbSet<Kullanici> kullanicilar { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var conStr = ("Server =. ; DataBase = KentKartOtoamasyonu ; User Id = Kullanici ; Password =1243;TrustServerCertificate = true");
+            optionsBuilder.UseSqlServer(conStr);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+}
